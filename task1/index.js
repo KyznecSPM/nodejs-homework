031d6c5a0ba2ed2ff2f7c13b26f08a0eebda0daf
Емельянов Дмитрий Олегович <dmitry_emelianov@epam.com>
2021-10-05 16:39:12 +0300

    add: linter, prettier, task1

---
+++ task1/index.js
@@ -0,0 +1,9 @@
+const process = require('process');
+
+const task1 = () => {
+  process.stdin.on('data', (data) => {
+    console.log(data.toString().split('').reverse().join(''));
+  });
+};
+
+task1();
