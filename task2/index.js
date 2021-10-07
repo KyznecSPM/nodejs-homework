6fe06aa9248b8c02f3f46cad0c0e0d9064a6fbe3
Емельянов Дмитрий Олегович <dmitry_emelianov@epam.com>
2021-10-07 10:13:24 +0300

    add: task2 solution

---
+++ task2/index.js
@@ -0,0 +1,45 @@
+const csv = require('csvtojson');
+const fs = require('fs');
+const path = require('path');
+
+const INPUT_FILE_PATH = 'csv/file.csv';
+const OUTPUT_FILE_PATH = 'output/file.txt';
+
+const OnError = (error) => console.error(error);
+const clearFile = () => {
+  try {
+    fs.writeFile(
+      path.resolve(__dirname, OUTPUT_FILE_PATH),
+      '',
+      'utf8',
+      (error) => error && console.error(error)
+    );
+  } catch (error) {}
+};
+
+const task2 = () => {
+  clearFile();
+  csv()
+    .fromStream(fs.createReadStream(path.resolve(__dirname, INPUT_FILE_PATH)))
+    .subscribe((json) => {
+      return new Promise((resolve) => {
+        try {
+          const lineWithoutAmount = JSON.stringify({
+            ...json,
+            Amount: undefined
+          });
+          fs.appendFile(
+            path.resolve(__dirname, OUTPUT_FILE_PATH),
+            `${lineWithoutAmount}\n`,
+            'utf8',
+            (error) => error && console.error(error)
+          );
+          resolve();
+        } catch (error) {
+          OnError(error);
+        }
+      });
+    }, OnError);
+};
+
+task2();
