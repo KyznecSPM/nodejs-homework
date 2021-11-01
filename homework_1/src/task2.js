147b5ef5f818b483f4d18832555d28953cc718ec
Емельянов Дмитрий Олегович <dmitry_emelianov@epam.com>
2021-11-01 15:06:54 +0300

    fix: homework_1 pipeline method; homework_2 delete mock

--- homework_1/src/task2.js
+++ homework_1/src/task2.js
@@ -2,57 +2,27 @@ import csv from 'csvtojson';
 import fs from 'fs';
 import path from 'path';
 
-const INPUT_FILE_PATH = '../../csv/file.csv';
-const OUTPUT_FILE_PATH = '../../output/file.txt';
+const INPUT_FILE_PATH = '../csv/file.csv';
+const OUTPUT_FILE_PATH = '../output/file.txt';
 
 const EXCLUDE_COLUMNS = ['Amount'];
 
-const readFileStream = fs.createReadStream(
+const readStream = fs.createReadStream(
   path.resolve(__dirname, INPUT_FILE_PATH)
 );
 
-const onError = (error) => console.error(error);
+const writeStream = fs.createWriteStream(
+  path.resolve(__dirname, OUTPUT_FILE_PATH)
+);
 
-const clearFile = () => {
-  try {
-    fs.writeFile(
-      path.resolve(__dirname, OUTPUT_FILE_PATH),
-      '',
-      'utf8',
-      (error) => error && console.error(error)
-    );
-  } catch (error) {
-    onError(error);
-  }
-};
-
-const getFilteredObject = (object, keys) =>
-  Object.entries(object).reduce((acc, [key, value]) => {
-    if (keys.includes(key)) return acc;
-    return { ...acc, [key]: value };
-  }, {});
-
-const saveFilteredJson = async (object, keys) => {
-  try {
-    const filteredString = JSON.stringify(getFilteredObject(object, keys));
-    await fs.appendFile(
-      path.resolve(__dirname, OUTPUT_FILE_PATH),
-      `${filteredString}\n`,
-      'utf8',
-      (error) => error && console.error(error)
-    );
-  } catch (error) {
-    onError(error);
-  }
-};
-
-const task2 = () => {
-  clearFile();
-  csv()
-    .fromStream(readFileStream)
-    .subscribe((object) => {
-      return saveFilteredJson(object, EXCLUDE_COLUMNS);
-    }, onError);
-};
-
-task2();
+readStream
+  .pipe(
+    csv().subscribe((csvLine) => {
+      return new Promise((resolve) => {
+        EXCLUDE_COLUMNS.map((columnName) => (csvLine[columnName] = undefined));
+        resolve();
+      });
+    })
+  )
+  .pipe(writeStream)
+  .on('error', (err) => console.error(err));
