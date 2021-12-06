4474f3d45c161dd38034b2b5c4698dec469a70f8
Емельянов Дмитрий Олегович <dmitry_emelianov@epam.com>
2021-12-06 21:39:41 +0300

    add: logger

--- homework_2_3/src/app.js
+++ homework_2_3/src/app.js
@@ -1,9 +1,48 @@
+import expressWinston from 'express-winston';
+import winston from 'winston';
+import methodOverride from 'method-override';
 import { server } from './config/server';
 import { database } from './config/database';
 import { userRouter, groupsRouter } from './api';
+import { PORT } from './config/constants';
+
+const logger = expressWinston.logger({
+  transports: [
+    new winston.transports.Console(),
+    new winston.transports.File({
+      filename: 'logs/data.log'
+    })
+  ],
+  format: winston.format.combine(
+    winston.format.colorize(),
+    winston.format.json()
+  )
+});
+
+const errorLogger = expressWinston.errorLogger({
+  transports: [
+    new winston.transports.Console(),
+    new winston.transports.File({
+      filename: 'logs/error.log'
+    })
+  ],
+  format: winston.format.combine(
+    winston.format.colorize(),
+    winston.format.json()
+  )
+});
+
+server.use(methodOverride());
+server.use(logger);
 
 server.use('/api/v1/users', userRouter);
 server.use('/api/v1/groups', groupsRouter);
+
+server.use(errorLogger);
+
+server.listen(PORT, () => {
+  console.log(`Server run on port ${PORT}`);
+});
 
 database
   .authenticate()
--- homework_2_3/src/config/server.js
+++ homework_2_3/src/config/server.js
@@ -1,12 +1,7 @@
 import express from 'express';
-import { PORT } from './constants';
 
 const server = express();
 
 server.use(express.json());
-
-server.listen(PORT, () => {
-  console.log(`Server run on port ${PORT}`);
-});
 
 export { server };
